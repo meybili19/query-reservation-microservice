ac6f942de1c6ab30c2cdd91c180a3a8100224037
Meybili <meybili200219@gmail.com>
2025-02-02 12:19:50 -0500

    feat: :sparkles: Change the reservstionService
    Change the reservation Service, reservation Resolver, reservation Schema, to get only one route, which is to get reservations by Id

--- src/resolvers/reservationResolver.js
+++ src/resolvers/reservationResolver.js
@@ -1,9 +1,6 @@
 const reservationService = require('../services/reservationService');
 
 const reservationResolver = {
-  getAllReservations: async () => {
-    return await reservationService.getAllReservations();
-  },
   getReservationById: async ({ id }) => {
     return await reservationService.getReservationById(id);
   },
